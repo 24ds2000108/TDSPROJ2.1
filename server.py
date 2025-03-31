a93190348173a871953ec5db96d95db5d643f68f
24ds2000108 <24ds2000108@ds.study.iitm.ac.in>
2025-03-31 23:26:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "FastAPI on Vercel!"}
-
-if _name_ == "_main_":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def home():
+    return {"message": "Hello, Vercel!"}
